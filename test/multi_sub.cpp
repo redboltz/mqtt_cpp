9c5c16f44c543ae551b307936702e320920076ca
Takatoshi Kondo <redboltz@gmail.com>
2017-12-18 22:28:25 +0900

    Refined cmake tree.
    Test cases are separeted individually.

--- test/multi_sub.cpp
+++ test/multi_sub.cpp
@@ -4,6 +4,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#include "test_main.hpp"
 #include "combi_test.hpp"
 
 BOOST_AUTO_TEST_SUITE(test_multi_sub)
