e54f2e4b76cd075cd33411d45895c8dcec2cb968
Takatoshi Kondo <redboltz@gmail.com>
2019-05-07 15:38:46 +0900

    Replaced BOOST_ASSERT with overwrite.
    It is corner case but BOOST_ASSERT is too strong.
    Overwrite is better solution.

--- include/mqtt/endpoint.hpp
+++ include/mqtt/endpoint.hpp
@@ -10172,7 +10172,24 @@ private:
                         control_packet_type::pubcomp,
                         msg
                     );
-                    BOOST_ASSERT(ret.second);
+                    // publish store is erased when pubrec is received.
+                    // pubrel store is erased when pubcomp is received.
+                    // If invalid client send pubrec twice with the same packet id,
+                    // then send corresponding pubrel twice is a possible client/server
+                    // implementation.
+                    // In this case, overwrite store_.
+                    if (!ret.second) {
+                        store_.modify(
+                            ret.first,
+                            [&] (auto& e) {
+                                e = store(
+                                    packet_id,
+                                    control_packet_type::pubcomp,
+                                    msg
+                                );
+                            }
+                        );
+                    }
                 }
 
                 if (serialize) {
