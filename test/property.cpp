29ea90959db4c21efb3751c910f3a35740903a98
Takatoshi Kondo <redboltz@gmail.com>
2019-06-17 13:42:26 +0900

    Removed C++17 feature.

--- test/property.cpp
+++ test/property.cpp
@@ -15,8 +15,8 @@
 BOOST_AUTO_TEST_SUITE(test_property)
 
 BOOST_AUTO_TEST_CASE( content_type_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::content_type, mqtt::v5::property::content_type_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::content_type_ref, mqtt::v5::property::content_type>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::content_type, mqtt::v5::property::content_type_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::content_type_ref, mqtt::v5::property::content_type>::value, "");
     mqtt::v5::property::content_type v1 { "abc" };
     mqtt::v5::property::content_type_ref v_ref1 { "abc" };
 
@@ -27,8 +27,8 @@ BOOST_AUTO_TEST_CASE( content_type_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( response_topic_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::response_topic, mqtt::v5::property::response_topic_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::response_topic_ref, mqtt::v5::property::response_topic>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::response_topic, mqtt::v5::property::response_topic_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::response_topic_ref, mqtt::v5::property::response_topic>::value, "");
     mqtt::v5::property::response_topic v1 { "abc" };
     mqtt::v5::property::response_topic_ref v_ref1 { "abc" };
 
@@ -39,8 +39,8 @@ BOOST_AUTO_TEST_CASE( response_topic_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( correlation_data_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::correlation_data, mqtt::v5::property::correlation_data_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::correlation_data_ref, mqtt::v5::property::correlation_data>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::correlation_data, mqtt::v5::property::correlation_data_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::correlation_data_ref, mqtt::v5::property::correlation_data>::value, "");
     mqtt::v5::property::correlation_data v1 { "abc" };
     mqtt::v5::property::correlation_data_ref v_ref1 { "abc" };
 
@@ -51,8 +51,8 @@ BOOST_AUTO_TEST_CASE( correlation_data_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( assigned_client_identifier_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::assigned_client_identifier, mqtt::v5::property::assigned_client_identifier_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::assigned_client_identifier_ref, mqtt::v5::property::assigned_client_identifier>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::assigned_client_identifier, mqtt::v5::property::assigned_client_identifier_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::assigned_client_identifier_ref, mqtt::v5::property::assigned_client_identifier>::value, "");
     mqtt::v5::property::assigned_client_identifier v1 { "abc" };
     mqtt::v5::property::assigned_client_identifier_ref v_ref1 { "abc" };
 
@@ -63,8 +63,8 @@ BOOST_AUTO_TEST_CASE( assigned_client_identifier_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( authentication_method_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::authentication_method, mqtt::v5::property::authentication_method_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::authentication_method_ref, mqtt::v5::property::authentication_method>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::authentication_method, mqtt::v5::property::authentication_method_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::authentication_method_ref, mqtt::v5::property::authentication_method>::value, "");
     mqtt::v5::property::authentication_method v1 { "abc" };
     mqtt::v5::property::authentication_method_ref v_ref1 { "abc" };
 
@@ -75,8 +75,8 @@ BOOST_AUTO_TEST_CASE( authentication_method_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( authentication_data_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::authentication_data, mqtt::v5::property::authentication_data_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::authentication_data_ref, mqtt::v5::property::authentication_data>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::authentication_data, mqtt::v5::property::authentication_data_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::authentication_data_ref, mqtt::v5::property::authentication_data>::value, "");
     mqtt::v5::property::authentication_data v1 { "abc" };
     mqtt::v5::property::authentication_data_ref v_ref1 { "abc" };
 
@@ -87,8 +87,8 @@ BOOST_AUTO_TEST_CASE( authentication_data_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( response_information_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::response_information, mqtt::v5::property::response_information_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::response_information_ref, mqtt::v5::property::response_information>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::response_information, mqtt::v5::property::response_information_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::response_information_ref, mqtt::v5::property::response_information>::value, "");
     mqtt::v5::property::response_information v1 { "abc" };
     mqtt::v5::property::response_information_ref v_ref1 { "abc" };
 
@@ -99,8 +99,8 @@ BOOST_AUTO_TEST_CASE( response_information_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( server_reference_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::server_reference, mqtt::v5::property::server_reference_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::server_reference_ref, mqtt::v5::property::server_reference>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::server_reference, mqtt::v5::property::server_reference_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::server_reference_ref, mqtt::v5::property::server_reference>::value, "");
     mqtt::v5::property::server_reference v1 { "abc" };
     mqtt::v5::property::server_reference_ref v_ref1 { "abc" };
 
@@ -111,8 +111,8 @@ BOOST_AUTO_TEST_CASE( server_reference_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( reason_string_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::reason_string, mqtt::v5::property::reason_string_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::reason_string_ref, mqtt::v5::property::reason_string>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::reason_string, mqtt::v5::property::reason_string_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::reason_string_ref, mqtt::v5::property::reason_string>::value, "");
     mqtt::v5::property::reason_string v1 { "abc" };
     mqtt::v5::property::reason_string_ref v_ref1 { "abc" };
 
@@ -123,8 +123,8 @@ BOOST_AUTO_TEST_CASE( reason_string_ref ) {
 }
 
 BOOST_AUTO_TEST_CASE( user_property_ref ) {
-    static_assert(std::is_convertible<mqtt::v5::property::user_property, mqtt::v5::property::user_property_ref>::value);
-    static_assert(std::is_convertible<mqtt::v5::property::user_property_ref, mqtt::v5::property::user_property>::value);
+    static_assert(std::is_convertible<mqtt::v5::property::user_property, mqtt::v5::property::user_property_ref>::value, "");
+    static_assert(std::is_convertible<mqtt::v5::property::user_property_ref, mqtt::v5::property::user_property>::value, "");
     mqtt::v5::property::user_property v1 { "abc", "def" };
     mqtt::v5::property::user_property_ref v_ref1 { "abc", "def" };
 
