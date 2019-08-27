1d950b521b572fc92be41e179d645af60d6c8582
Takatoshi Kondo <redboltz@gmail.com>
2019-08-27 14:59:18 +0900

    Added tests.

--- test/as_buffer_async_pubsub_2.cpp
+++ test/as_buffer_async_pubsub_2.cpp
@@ -870,6 +870,209 @@ BOOST_AUTO_TEST_CASE( publish_function ) {
     do_combi_test_async(test);
 }
 
+BOOST_AUTO_TEST_CASE( publish_function_buffer ) {
+    auto test = [](boost::asio::io_service& ios, auto& c, auto& s, auto& /*b*/) {
+        using packet_id_t = typename std::remove_reference_t<decltype(*c)>::packet_id_t;
+        c->set_clean_session(true);
+
+        std::uint16_t pid_sub;
+        std::uint16_t pid_unsub;
+
+
+        checker chk = {
+            // connect
+            cont("h_connack"),
+            // subscribe topic1 QoS0
+            cont("h_suback"),
+            // publish topic1 QoS0
+            cont("h_publish"),
+            cont("h_unsuback"),
+            // disconnect
+            cont("h_close"),
+        };
+
+        switch (c->get_protocol_version()) {
+        case MQTT_NS::protocol_version::v3_1_1:
+            c->set_connack_handler(
+                [&chk, &c, &pid_sub]
+                (bool sp, std::uint8_t connack_return_code) {
+                    MQTT_CHK("h_connack");
+                    BOOST_TEST(sp == false);
+                    BOOST_TEST(connack_return_code == MQTT_NS::connect_return_code::accepted);
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    pid_sub = c->async_subscribe(
+                        std::move(topic1),
+                        MQTT_NS::qos::at_most_once,
+                        [topic1](boost::system::error_code const&) {}
+                    );
+                    return true;
+                });
+            c->set_puback_handler(
+                []
+                (std::uint16_t) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_pubrec_handler(
+                []
+                (std::uint16_t) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_pubcomp_handler(
+                []
+                (std::uint16_t) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_suback_handler(
+                [&chk, &c, &pid_sub]
+                (packet_id_t packet_id, std::vector<MQTT_NS::optional<std::uint8_t>> results) {
+                    MQTT_CHK("h_suback");
+                    BOOST_TEST(packet_id == pid_sub);
+                    BOOST_TEST(results.size() == 1U);
+                    BOOST_TEST(*results[0] == MQTT_NS::qos::at_most_once);
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    auto contents = MQTT_NS::allocate_buffer("topic1_contents");
+                    c->async_publish(
+                        std::move(topic1),
+                        std::move(contents),
+                        MQTT_NS::qos::at_most_once
+                    );
+                    return true;
+                });
+            c->set_unsuback_handler(
+                [&chk, &c, &pid_unsub]
+                (packet_id_t packet_id) {
+                    MQTT_CHK("h_unsuback");
+                    BOOST_TEST(packet_id == pid_unsub);
+                    c->async_disconnect();
+                    return true;
+                });
+            c->set_publish_handler(
+                [&chk, &c, &pid_unsub]
+                (std::uint8_t header,
+                 MQTT_NS::optional<packet_id_t> packet_id,
+                 MQTT_NS::string_view topic,
+                 MQTT_NS::string_view contents) {
+                    MQTT_CHK("h_publish");
+                    BOOST_TEST(MQTT_NS::publish::is_dup(header) == false);
+                    BOOST_TEST(MQTT_NS::publish::get_qos(header) == MQTT_NS::qos::at_most_once);
+                    BOOST_TEST(MQTT_NS::publish::is_retain(header) == false);
+                    BOOST_CHECK(!packet_id);
+                    BOOST_TEST(topic == "topic1");
+                    BOOST_TEST(contents == "topic1_contents");
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    pid_unsub = c->async_unsubscribe(
+                        std::move(topic1),
+                        [topic1](boost::system::error_code const&) {}
+                    );
+                    return true;
+                });
+            break;
+        case MQTT_NS::protocol_version::v5:
+            c->set_v5_connack_handler(
+                [&chk, &c, &pid_sub]
+                (bool sp, std::uint8_t connack_return_code, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    MQTT_CHK("h_connack");
+                    BOOST_TEST(sp == false);
+                    BOOST_TEST(connack_return_code == MQTT_NS::connect_return_code::accepted);
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    pid_sub = c->async_subscribe(
+                        std::move(topic1),
+                        MQTT_NS::qos::at_most_once,
+                        [topic1](boost::system::error_code const&) {}
+                    );
+                    return true;
+                });
+            c->set_v5_puback_handler(
+                []
+                (packet_id_t, std::uint8_t, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_v5_pubrec_handler(
+                []
+                (packet_id_t, std::uint8_t, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_v5_pubcomp_handler(
+                []
+                (packet_id_t, std::uint8_t, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    BOOST_CHECK(false);
+                    return true;
+                });
+            c->set_v5_suback_handler(
+                [&chk, &c, &pid_sub]
+                (packet_id_t packet_id, std::vector<std::uint8_t> reasons, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    MQTT_CHK("h_suback");
+                    BOOST_TEST(packet_id == pid_sub);
+                    BOOST_TEST(reasons.size() == 1U);
+                    BOOST_TEST(reasons[0] == MQTT_NS::v5::reason_code::granted_qos_0);
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    auto contents = MQTT_NS::allocate_buffer("topic1_contents");
+                    c->async_publish(
+                        std::move(topic1),
+                        std::move(contents),
+                        MQTT_NS::qos::at_most_once
+                    );
+                    return true;
+                });
+            c->set_v5_unsuback_handler(
+                [&chk, &c, &pid_unsub]
+                (packet_id_t packet_id, std::vector<std::uint8_t> reasons, std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    MQTT_CHK("h_unsuback");
+                    BOOST_TEST(packet_id == pid_unsub);
+                    BOOST_TEST(reasons.size() == 1U);
+                    BOOST_TEST(reasons[0] == MQTT_NS::v5::reason_code::success);
+                    c->async_disconnect();
+                    return true;
+                });
+            c->set_v5_publish_handler(
+                [&chk, &c, &pid_unsub]
+                (std::uint8_t header,
+                 MQTT_NS::optional<packet_id_t> packet_id,
+                 MQTT_NS::string_view topic,
+                 MQTT_NS::string_view contents,
+                 std::vector<MQTT_NS::v5::property_variant> /*props*/) {
+                    MQTT_CHK("h_publish");
+                    BOOST_TEST(MQTT_NS::publish::is_dup(header) == false);
+                    BOOST_TEST(MQTT_NS::publish::get_qos(header) == MQTT_NS::qos::at_most_once);
+                    BOOST_TEST(MQTT_NS::publish::is_retain(header) == false);
+                    BOOST_CHECK(!packet_id);
+                    BOOST_TEST(topic == "topic1");
+                    BOOST_TEST(contents == "topic1_contents");
+                    auto topic1 = MQTT_NS::allocate_buffer("topic1");
+                    pid_unsub = c->async_unsubscribe(
+                        std::move(topic1),
+                        [topic1](boost::system::error_code const&) {}
+                    );
+                    return true;
+                });
+            break;
+        default:
+            BOOST_CHECK(false);
+            break;
+        }
+        c->set_close_handler(
+            [&chk, &s]
+            () {
+                MQTT_CHK("h_close");
+                s.close();
+            });
+        c->set_error_handler(
+            []
+            (boost::system::error_code const&) {
+                BOOST_CHECK(false);
+            });
+        c->connect();
+        ios.run();
+        BOOST_TEST(chk.all());
+    };
+    do_combi_test_async(test);
+}
+
 BOOST_AUTO_TEST_CASE( publish_dup_function ) {
     auto test = [](boost::asio::io_service& ios, auto& c, auto& s, auto& /*b*/) {
         using packet_id_t = typename std::remove_reference_t<decltype(*c)>::packet_id_t;
